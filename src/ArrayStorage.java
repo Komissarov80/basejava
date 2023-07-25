5f2f0094a8f18053ca24a68b7b208e0ad71f8bf7
VitalY <simferopol.jurart@gmail.com>
2023-07-25 18:06:38 +0300

    lesson01. fix 1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -4,32 +4,21 @@
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
     private int size;
-    private int cursor = -1;
-
-    public ArrayStorage() {
-        size = 0;
-    }
-
-    public int getCursor() {
-        return cursor;
-    }
 
     void clear() {
         for (int i = 0; i < size; i++) {
             storage[i] = null;
         }
         size = 0;
-        cursor = -1;
     }
 
     void save(Resume r) {
-        storage[++cursor] = r;
-        size++;
+        storage[size++] = r;
     }
 
     Resume get(String uuid) {
         for (int i = 0; i < size; i++) {
-            if(storage[i].uuid.equals(uuid)) {
+            if (storage[i].uuid.equals(uuid)) {
                 return storage[i];
             }
         }
@@ -38,17 +27,9 @@ public class ArrayStorage {
 
     void delete(String uuid) {
         for (int i = 0; i < size; i++) {
-            if(storage[i].uuid.equals(uuid)) {
-                if(i == size - 1) {
-                    storage[i] = null;
-                    size--;
-                    cursor--;
-                } else {
-                    System.arraycopy(storage, (i + 1), storage, i, (size - i - 1));
-                    storage[size - 1] = null;
-                    size--;
-                    cursor--;
-                }
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = storage[size - 1];
+                storage[--size] = null;
             }
         }
     }
@@ -57,11 +38,11 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        Resume[] resultsResume = new Resume[size];
+        Resume[] resumes = new Resume[size];
         for (int i = 0; i < size; i++) {
-            resultsResume[i] = storage[i];
+            resumes[i] = storage[i];
         }
-        return resultsResume;
+        return resumes;
     }
 
     int size() {
